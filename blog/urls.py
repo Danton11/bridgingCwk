cc8f0c3753301d66880cfcad0d94996e5c19f85c
Daniel <danielbenton909@gmail.com>
2020-06-09 13:08:34 +0100

    Files

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
+from .views import ArticleDetailView
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
+    path('article/<int:pk>',ArticleDetailView.as_view(),name='article-detail'),
 ]
